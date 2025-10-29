a139ea62796f68dde49e40f338482b48eb92aaee
Darko2504 <darkomilanovski45@gmail.com>
2025-10-29 21:20:49 +0100

    MapperConfig added

---
+++ BurgerStoreServer/BurgerStore.Dtos/UserDtos/UserDto.cs
@@ -0,0 +1,8 @@
+﻿using BurgerStore.Domain.Entities;
+
+namespace BurgerStore.Dtos.UserDtos
+{
+    public class UserDto : User
+    {
+    }
+}
---
+++ BurgerStoreServer/BurgerStore.Mapperss/MapperConfig/AutoMapperProfile.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using BurgerStore.Domain.Entities;
+using BurgerStore.Dtos.BurgerDtos;
+using BurgerStore.Dtos.OrderDtos;
+using BurgerStore.Dtos.UserDtos;
+using Microsoft.AspNetCore.Identity;
+using System.Runtime.CompilerServices;
+
+namespace BurgerStore.Mapperss.MapperConfig
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<User, LoginUserRequestDto>().ReverseMap();
+            CreateMap<User, UserOrderDto>().ReverseMap();
+            CreateMap<User, RegisterUserRequestDto>().ReverseMap();
+            CreateMap<User, UpdateUserDto>().ReverseMap();
+            CreateMap<User, UserDto>().ReverseMap();
+
+            CreateMap<Burger, BurgerDto>().ReverseMap();
+            CreateMap<Burger, AddBurgerDto>().ReverseMap();
+            CreateMap<Burger, UpdateBurgerDto>().ReverseMap();
+            CreateMap<Burger, AddBurgerDto>().ReverseMap();
+
+            CreateMap<Order, OrderDto>().ReverseMap();
+            CreateMap<Order, AdOrderDto>().ReverseMap();
+            CreateMap<Order, UpdateOrderDto>().ReverseMap();
+
+        }
+    }
+}
